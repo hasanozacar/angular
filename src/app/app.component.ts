d8508040d45e6653d0e88000118e7c7cb76126b1
= <130917353+Teknomina@users.noreply.github.com>
2023-06-14 21:28:38 +0300

    One way data  and event binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  tasks =[
+    "Visit",
+    "Call",
+    "Gym"
+  ]
+  add(value : String){
+    alert(value)
+  }
+
+
 }
